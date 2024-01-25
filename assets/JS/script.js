4d0897febb005a1b65e14ac6dd0f57f3572f5727
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-01-25 00:08:22 +0000

    bootstrap added for forecast

---
+++ assets/JS/script.js
@@ -0,0 +1,6 @@
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+const weatherApiKey = "700c1c890919726aec2084f550e46b49";
+
+var cityEl = $(".city");
+var dateEl = $(".date");
+var tempEl = $(".temp");
