29afebdd6b4525557982a271f3436e239653b563
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-01-27 21:45:43 +0000

    working with js, data displaying on page for current day

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,52 @@
-const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+var cityName = "leeds";
+// get api
 const weatherApiKey = "700c1c890919726aec2084f550e46b49";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + weatherApiKey + "&units=metric";
 
-var cityEl = $(".city");
-var dateEl = $(".date");
-var tempEl = $(".temp");
+fetch(weatherUrl)
+  .then(function (response) {
+    if (response.status === 404) {
+      $(".error").css("display", "block");
+      $(".weather").css("display", "none");
+    }
+
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+
+    $(".search-button").on("click", function (event) {
+      event.preventDefault();
+      console.log("clock");
+    });
+    //current weather display elements
+    var cityEl = $("#location");
+    var dateEl = $("#current-date");
+    var tempEl = $("#temp");
+    var windSpeedEl = $("#wind-speed");
+    var humidityEl = $("#humidity");
+
+    // target display with data
+    cityEl.text(data.name);
+    dateEl.text(dayjs().format("DD/MM/YYYY"));
+    tempEl.text(parseInt(data.main.temp) + " °c");
+    windSpeedEl.text("Wind Speed: " + data.wind.speed + " m/s");
+    humidityEl.text("Humidity: " + data.main.humidity + "%");
+
+    // change image to display current weather
+    var weatherIcon = $(".current-weather-icon");
+
+    if (data.weather[0].main == "Clear") {
+      weatherIcon.attr("src", "assets/images/clear.png");
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.attr("src", "assets/images/rain.png");
+    } else if (data.weather[0].main == "Clouds") {
+      weatherIcon.attr("src", "assets/images/clouds.png");
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.attr("src", "assets/images/mist.png");
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.attr("src", "assets/images/snow.png");
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.attr("src", "assets/images/drizzle.png");
+    }
+  });
