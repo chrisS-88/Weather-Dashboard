fc9b8d949781f0f546a224d4139bf1ad3af6e43a
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-01-29 14:59:13 +0000

    local storge working and 5 day forecast working

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,16 +1,21 @@
 // $(".weather").css("display", "none");
 
-$("#search-button").on("click", function (event) {
-  event.preventDefault();
+$("#search-button").on("click", getWeather);
 
+function getWeather(event) {
+  event.preventDefault();
   // grab user input
   var userInput = $("#search-input").val().trim();
-  console.log(userInput);
+
+  // prepend search to history section
+  var btnEl = $("<button>");
+  btnEl.text(userInput);
+  $("#history").prepend(btnEl);
 
   // save search to local storage
+  saveUerInput();
 
-  localStorage.setItem("location", userInput);
-
+  // grab api for weather data
   const weatherApiKey = "700c1c890919726aec2084f550e46b49";
   const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + weatherApiKey + "&units=metric";
 
@@ -25,7 +30,7 @@ $("#search-button").on("click", function (event) {
       }
     })
     .then(function (data) {
-      console.log(data);
+      // console.log(data);
 
       //current weather display elements
       var cityEl = $("#location");
@@ -59,5 +64,77 @@ $("#search-button").on("click", function (event) {
       }
 
       $(".weather").css("display", "block");
+
+      // 5 day forecast
+      var forecastEl = $("#forecast");
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      var forecast =
+        "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + weatherApiKey + "&units=metric";
+
+      fetch(forecast)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          // console.log(data);
+          var forcastList = data.list;
+
+          for (let i = 0; i < forcastList.length; i += 8) {
+            var { dt_txt, weather, main, wind, humidity } = forcastList[i];
+            date = dt_txt.split(" ");
+            var iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
+            temp = parseInt(main.temp);
+            wind = wind.speed;
+            humidity = main.humidity;
+
+            forecastEl.append(`
+            <div class="col-md-2 col-lg-2 mx-auto card mb-3">
+              <div class="text-start card-body p-2">
+                <h5 class="card-title">${date[0]}</h5>
+                <img class="card-text" src=${iconUrl} > 
+                <p class="card-text">Temp: ${temp} °c</p>
+                <p class="card-text">Wind: ${wind} m/s</p>
+                <p class="card-text">Humidity: ${humidity} %</p>
+              </div>
+            `);
+          }
+        });
     });
-});
+}
+
+// functions to save user input to local storage
+function saveTolocalStorage(key, value) {
+  let existingData = JSON.parse(localStorage.getItem(key)) || [];
+
+  existingData.push(value);
+  localStorage.setItem(key, JSON.stringify(existingData));
+}
+
+function saveUerInput() {
+  var userInput = $("#search-input").val().trim();
+
+  if (userInput !== "") {
+    saveTolocalStorage("location", userInput);
+  }
+}
+
+// get history from local storage and display on refresh
+function getHistory() {
+  var storedResults = localStorage.getItem("location");
+  console.log(storedResults);
+  if (storedResults !== null) {
+    var items = JSON.parse(storedResults);
+  }
+  displayLocationHistory(items);
+}
+
+function displayLocationHistory(items) {
+  items.forEach(function (item) {
+    var btnEl = $("<button>");
+    btnEl.text(item);
+    $("#history").prepend(btnEl);
+  });
+}
+getHistory();
